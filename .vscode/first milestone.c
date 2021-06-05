287d68b2650d4d992bf3efd2c329f3a37e37ea72
dina12118 <85133414+dina12118@users.noreply.github.com>
2021-06-05 23:28:50 +0200

    Update first milestone.c

--- .vscode/first milestone.c
+++ .vscode/first milestone.c
@@ -72,4 +72,14 @@ if ((GPIO_PORTF_DATA_R&0x11)==0x10) { // switch of pin 4 to end calculation of t
            return totalDistance; // Return toatal distance 
 }
 }
+    void LCD_COMM(unsigned char command)
+{
+GPIO_PORTA_DATA_R = 0x00; //RS =0, E=0, RW=0
+GPIO_PORTB_DATA_R =commnd; //missed a
+GPIO_PORTA_DATA_R =0x80; //E=1 to secure command
+delay_micro(0);
+GPIO_PORTA_DATA_R =0x00;
+if(command <4) delay_milli(2); else delay_micro(37);
+    }
+}
 }
