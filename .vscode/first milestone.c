7b44004124144cf133b6652e0b4a8c2ae6dd86cd
Ahmedsaid112 <78927847+Ahmedsaid112@users.noreply.github.com>
2021-06-05 19:55:01 +0200

    Update first milestone.c

--- .vscode/first milestone.c
+++ .vscode/first milestone.c
@@ -15,14 +15,17 @@ SYSCTL_RCGCGPIO_R |=0x20 ;
 while((SYSCTL_PRGPIO_R & 0x20)==0){};
 
 GPIO_PORTF_LOCK_R=0x4C4F434B; 
-GPIO_PORTF_CR_R=; //on for all pins
-GPIO_PORTF_AFSEL_R=; //no AF
-GPIO_PORTF_PCTL_R=;  //all pins GPIO
-GPIO_PORTF_AMSEL_R=; // all digital
-GPIO_PORTF_DIR_R=; //f1 & f2 & f3 output 
-GPIO_PORTF_DEN_R=; //all pins digital
-GPIO_PORTF_PUR_R=; // on for first and last pin
-//GPIO_PORTF_DATA_R&=0x0E;
+GPIO_PORTF_CR_R=0x1F; //on for all pins
+GPIO_PORTF_AFSEL_R=0; //no AF
+GPIO_PORTF_PCTL_R=0;  //all pins GPIO
+GPIO_PORTF_AMSEL_R=0; // all digital
+GPIO_PORTF_DIR_R=0x0E; //f1 & f2 & f3 output 
+GPIO_PORTF_DEN_R=0X1F; //all pins digital
+GPIO_PORTF_PUR_R=0x11; // on for first and last pin
+
+
+
+
 
 
 }
